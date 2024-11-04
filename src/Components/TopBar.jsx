da2f257e7b1eae1db1dbedd8b6c544875b05b1a0
Ravi Pratihast <ravipratihast@gmail.com>
2024-11-04 21:49:56 +0530

    Test: yet to be tested.

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -3,9 +3,9 @@ import { Avatar } from "./Avatar";
 
 const TopBar = () => {
   return (
-    <div className="fixed top-[3.7rem] right-[6rem] w-[9.3rem] h-[4.1rem] - flex justify-between items-center">
-      <GoBellFill className="text-3xl h w-auto h-auto  bg-[#E7F8FF] px-4 py-3 rounded-md" />
-      <Avatar />
+    <div className=" w-[100%] h-[4.1rem] flex border justify-between items-center border-red-500">
+      {/* <GoBellFill className="text-3xl h w-auto h-auto  bg-[#E7F8FF] px-4 py-3 rounded-md" />
+      <Avatar /> */}
     </div>
   );
 };
