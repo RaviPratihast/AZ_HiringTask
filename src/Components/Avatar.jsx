ce355c2383271a0d38ab455167b7c703b9ac3723
Ravi Pratihast <ravipratihast@gmail.com>
2024-11-04 09:35:08 +0530

    Refactor: Avatar component

--- src/Components/Avatar.jsx
+++ src/Components/Avatar.jsx
@@ -1,8 +1,6 @@
 const Avatar = () => {
   return (
-    <div className="w-[3.9rem] h-[3.9rem] bg-[#D9D9D9] rounded-full">
-      {/* <img alt="Avatar" className="w-full h-full object-cover" /> */}
-    </div>
+    <div className="w-[3.9rem] h-[3.9rem] bg-[#D9D9D9] rounded-full"></div>
   );
 };
 
