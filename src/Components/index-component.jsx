c55a381c07114ff1e8ff030151db6508b5e32c4f
Ravi Pratihast <ravipratihast@gmail.com>
2024-11-04 21:55:49 +0530

    Add: Content related components are added.

--- src/Components/index-component.jsx
+++ src/Components/index-component.jsx
@@ -1,5 +1,18 @@
 import { Sidebar } from "./Sidebar";
 import { TopBar } from "./TopBar";
 import { Avatar } from "./Avatar";
-export { Sidebar, TopBar, Avatar };
-            
+import { Content } from "./Content";
+import { Tabs } from "./Tabs";
+import { PartSection } from "./PartSection";
+import { ResourceItem } from "./ResourceItem";
+import { CourseContent } from "./CourseContent";
+export {
+  Sidebar,
+  TopBar,
+  Avatar,
+  Content,
+  Tabs,
+  PartSection,
+  ResourceItem,
+  CourseContent,
+};
